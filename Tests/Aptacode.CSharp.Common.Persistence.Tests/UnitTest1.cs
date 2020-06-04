ac3ddd38985994d298a7cda726c019b7db8560fe
Timothy Jones <t.jones@aptacode.com>
2020-06-04 18:00:35 +0100

    Created Empty Test projects

---
+++ Tests/Aptacode.CSharp.Common.Persistence.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Aptacode.CSharp.Common.Persistence.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
