6299251a1f475138d76cc7746317f00958040e8f
Timothy Jones <t.jones@aptacode.com>
2020-06-11 19:48:18 +0100

    Added BindableBase Tests

---
+++ Tests/Aptacode.CSharp.Common.Utillities.Tests/Mvvm/BindableBaseTests.cs
@@ -0,0 +1,43 @@
+﻿using Aptacode.CSharp.Common.Utilities.Mvvm;
+using Xunit;
+
+namespace Aptacode.CSharp.Common.Utilities.Tests.Mvvm
+{
+    public class BindableBaseTests
+    {
+
+        private class FakeBindableBase : BindableBase
+        {
+            private int _testProperty;
+
+            public int TestProperty
+            {
+                get => _testProperty;
+                set => SetProperty(ref _testProperty, value);
+            }
+
+        }
+
+        [Fact]
+        public void PropertyChangedFires()
+        {
+            //Arrange
+            var propertyChangedFired = false;
+            var sut = new FakeBindableBase();
+
+            sut.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == nameof(sut.TestProperty))
+                {
+                    propertyChangedFired = true;
+                }
+            };
+
+            //Act
+            sut.TestProperty = 10;
+
+            //Assert
+            Assert.True(propertyChangedFired);
+        }
+    }
+}
