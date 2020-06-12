5ea08f2e9c74e3e3501d285c8a90d3f115605f3b
Timothy Jones <t.jones@aptacode.com>
2020-06-12 15:09:33 +0100

    Added Generic HttpRestService

---
+++ Aptacode.CSharp.Common.Http/Services/HttpRestService.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Aptacode.CSharp.Common.Http.Interfaces;
+using Aptacode.CSharp.Common.Http.Services.Responses;
+using AutoMapper;
+
+namespace Aptacode.CSharp.Common.Http.Services
+{
+    public class HttpRestService<T> : HttpRestService<T, T>
+    {
+        public HttpRestService(IHttpServiceClient serviceClient, IRouteProvider routeProvider, IMapper mapper) :
+            base(serviceClient, routeProvider, mapper) { }
+    }
+
+    public class HttpRestService<TResponse, TRequest>
+    {
+        protected readonly IMapper Mapper;
+        protected readonly IRouteProvider RouteProvider;
+        protected readonly IHttpServiceClient ServiceClient;
+
+        public HttpRestService(IHttpServiceClient serviceClient, IRouteProvider routeProvider, IMapper mapper)
+        {
+            ServiceClient = serviceClient;
+            RouteProvider = routeProvider;
+            Mapper = mapper;
+        }
+
+        public async Task<HttpServiceResponse<IEnumerable<TResponse>>> GetAll() =>
+            await ServiceClient.Send<IEnumerable<TResponse>>(HttpMethod.Get, RouteProvider.Build()).ConfigureAwait(false);
+
+        public async Task<HttpServiceResponse<TResponse>> Get(int id) =>
+            await ServiceClient.Send<TResponse>(HttpMethod.Get, RouteProvider.Build(id)).ConfigureAwait(false);
+
+        public async Task<HttpServiceResponse<TResponse>> Update(int id, TRequest content) =>
+            await ServiceClient.Send<TResponse, TRequest>(HttpMethod.Post, RouteProvider.Build(id), content).ConfigureAwait(false);
+
+        public async Task<HttpServiceResponse<TResponse>> Create(TRequest content) =>
+            await ServiceClient.Send<TResponse, TRequest>(HttpMethod.Put, RouteProvider.Build(), content).ConfigureAwait(false);
+
+        public async Task<HttpServiceResponse<bool>> Delete(int id) =>
+            await ServiceClient.Send<bool>(HttpMethod.Delete, RouteProvider.Build(id)).ConfigureAwait(false);
+    }
+}
